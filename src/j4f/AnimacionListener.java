5c1b1afda7c107312e0e3f462140113f36c0bac7
Luis Rodriguez <ballestax@gmail.com>
2019-05-05 21:57:34 -0500

    First commit.

--- src/j4f/AnimacionListener.java
+++ src/j4f/AnimacionListener.java
@@ -7,7 +7,6 @@ import java.util.EventListener;
  * @author ballestas
  */
 public interface AnimacionListener extends EventListener {
-
     public abstract void animacionStarted(AnimacionEvent animacionevent);
 
     public abstract void animacionStoped(AnimacionEvent animacionevent);
